e16cb9d89e91b91d59074ff6e94473921bd98f3d
Youngki Kang <yellowgg2@gmail.com>
2017-04-24 16:49:04 +0900

    parse clien

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 const clien = require("./src/parse_site");
 const config = require("./src/config");
-var i = 1;
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(config.token, {polling: true});
+const startParse = new clien.ParseClien(config.jirum);
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const opts = {
+        reply_to_message_id: msg.message_id,
+        reply_markup: {
+            resize_keyboard: true,
+            one_time_keyboard: true,
+            remove_keyboard: true,
+            // keyboard: [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
+        }
+    };
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, "Hello World", opts);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 
 var infiniteLoop = () => {
+  startParse.getJsonFromBody();
+
   if (startParse.articles != undefined) {
     console.log('[KangLOG] pageData : ' + JSON.stringify(startParse.articles.get()));
     return false;
   }
   else {
     console.log('[KangLOG] undefined : ');
-  }
-  i++;
-  if (i < 10) {
-    myLoop();
-  }
+  }  
+  getClienData();
 }
 
-function myLoop() {
+function getClienData() {
   setTimeout(infiniteLoop, 1000)
 }
 
-const startParse = new clien.ParseClien(config.jirum);
-startParse.getJsonFromBody();
-myLoop();
+getClienData();
