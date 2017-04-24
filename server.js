72e7809756e9b6aee943618d447c11eeaceb5f23
Youngki Kang <yellowgg2@gmail.com>
2017-04-24 12:09:34 +0900

    parse site

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const cheerio = require("cheerio");
 request("http://www.clien.net/cs2/bbs/board.php?bo_table=jirum", function (err, body) {
   let htmlBody = cheerio.load(err || body);
   // console.log(htmlBody("td.post_subject"));
-  let articles = htmlBody("tr.mytr");
-  var pageData = {};
+  let articles = htmlBody("tr.mytr");  
   
   articles.each((i, elem) => {
-    console.log(htmlBody(elem).children(".post_subject").text());
-    return false;
+    var pageData = {};
+    pageData["id"] = htmlBody(elem).children().html();
+    pageData["post_category"] = htmlBody(elem).children(".post_category").text();
+    pageData["post_subject"] = htmlBody(elem).children(".post_subject").text();
+    pageData["post_name"] = htmlBody(elem).children(".post_name").text();
+
+    console.log('[KangLOG] pageData : ' + JSON.stringify(pageData));
   })
 });
